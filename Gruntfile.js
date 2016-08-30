b118b1b67c7a960c7991744b417a64518286d2d8
MD-BPP <matthewdenny@bpp.com>
2016-08-30 11:20:20 +0100

    v1.0
    Initial build for hosting

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+      watch: {
+          scripts: {
+            files: ['css/*.scss', 'js/*.js'],
+            tasks: ['sass', 'uglify'],
+            options: {
+              spawn: false,
+            },
+          },
+        },
+    uglify: {
+      build: {
+        src: 'js/*.js',
+        dest: 'build/js/script.min.js'
+      }
+    },
+      sass: {                              // Task
+        dist: {                            // Target
+          options: {                       // Target options
+            style: 'compressed'
+          },
+          files: {                         // Dictionary of files
+            'build/css/style.min.css': 'css/style.scss'
+          }
+        }
+      }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'sass', 'watch']);
+
+};
